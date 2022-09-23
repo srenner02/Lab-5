4b0a1c065bdb01ca3780212d92e60b2d23420b1b
Samuel Renner <113732701+srenner02@users.noreply.github.com>
2022-09-23 22:56:08 +0000

    Added comments for JS

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ window.onload = init;
 		function init() {
 			addLastUpdate();
 		}
-//updates variable modDate with the date the document was last modified
-function addLastUpdate(){
-  var modDate = document.lastModified;
-  document.getElementById("update_date").innerHTML = modDate;
+//Function to display last modified in footer
+function addLastUpdate(){ 
+  //from w3schools.com/jsref/prop_doc_lastmodified.asp
+  const date = new Date(document.lastModified); 
+  //gets last_modified element and converts to toDateString
+  document.getElementById("last_modified").innerHTML = "Last Modified " + date.toDateString();
 }
-
+//validate function for the marital status radio
 function validateForm() {
+  //pulls status value from lab 5 form and assigns to x
   let x = document.forms["lab5Form"]["status"].value;
+  //if x is blank(no option chosen) displays alert
   if (x == "") {
     alert("Marital Status must be selected");
     return false;
